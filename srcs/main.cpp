faa643f84f6bdbee62c363dcb8cab04d20706445
Laure MASSON <lmasson@e1r6p7.clusters.42paris.fr>
2022-11-23 14:19:29 +0100

    check arg du main

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,5 +1,18 @@
 #include "../includes/include.hpp"
 
+int		isArgValid(char **argv)
+{
+	if (!isNumber(argv[1]))
+		return 0;
+	int port = std::atoi(argv[1]);
+	if (port < 0 || 65535 < port)
+	{
+		std::cout << "The port must be between 0 and 2^16 -1" << std::endl;
+		return 0;
+	}
+	return 1;
+}
+
 int		main(int argc, char **argv)
 {
 	if (argc != 3)
@@ -7,6 +20,8 @@ int		main(int argc, char **argv)
 		std::cerr << "To launch IRC : ./ircserv <port> <password>" << std::endl;
 		return (1);
 	}
+	if (!isArgValid(argv))
+		return (1);
 	int			port;
 	std::string	pwd(argv[2]);
 	port = std::atoi(argv[1]); // TODO ajouter un check sur le port
